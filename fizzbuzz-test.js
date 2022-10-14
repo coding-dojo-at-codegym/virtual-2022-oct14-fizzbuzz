4836c6b3c49eaf0d448d351f56371c970832ea49
thien <thien.nguyen@codegym.vn>
2022-10-14 21:02:42 +0700

    text input 3 return fizz

--- fizzbuzz-test.js
+++ fizzbuzz-test.js
@@ -1,8 +1,17 @@
 QUnit.module('fizzbuzz', function () {
-  QUnit.test('normal number', function (assert) {
-    const number = 1
-    const expected = "1"
-    const actual = fizzbuzz(number)
-    assert.equal(expected, actual)
-  })
+    QUnit.test('normal number', function (assert) {
+        const number = 1
+        const expected = "1"
+        const actual = fizzbuzz(number)
+        assert.equal(expected, actual)
+    })
+})
+
+QUnit.module('fizzbuzz', function () {
+    QUnit.test('text input 3 return fizz', function (assert) {
+        const number = 3
+        const expected = "fizz"
+        const actual = fizzbuzz(number)
+        assert.equal(expected, actual)
+    })
 })
