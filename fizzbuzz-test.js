6f220d31c1211c81f330924dfb41c134329cecaf
son <son@codegym.vn>
2022-10-14 20:32:50 +0700

    normal number test

--- fizzbuzz-test.js
+++ fizzbuzz-test.js
@@ -1,5 +1,8 @@
-QUnit.module('add', function () {
-  QUnit.test('two numbers', function (assert) {
-    assert.equal(add(1, 2), 3)
+QUnit.module('fizzbuzz', function () {
+  QUnit.test('normal number', function (assert) {
+    const number = 1
+    const expected = "1"
+    const actual = fizzbuzz(number)
+    assert.equal(expected, actual)
   })
 })
