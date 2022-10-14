1678ab809d5344c49068c04724d502b46c3fae02
lê tuấn <tuan.le@codegym.vn>
2022-10-14 21:44:13 +0700

    pass input 15 return fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,19 +1,21 @@
-const FIZZ = 'fizz'
-const BUZZ = 'buzz'
+const FIZZ = 'fizz';
+const BUZZ = 'buzz';
 
-function fizzbuzz (number) {
-  if (this.isFizz(number)) {
-    return FIZZ
-  } else if (this.isBuzz(number)) {
-    return BUZZ
-  }
-  return number
+function fizzbuzz(number) {
+    if (this.isFizz(number) && this.isBuzz(number)) {
+        return 'fizzbuzz';
+    } else if (this.isBuzz(number)) {
+        return BUZZ;
+    } else if (this.isFizz(number)) {
+        return FIZZ;
+    }
+    return number;
 }
 
-function isFizz (number) {
-  return number % 3 === 0
+function isFizz(number) {
+    return number % 3 === 0;
 }
 
-function isBuzz (number) {
-  return number % 5 === 0
+function isBuzz(number) {
+    return number % 5 === 0;
 }
