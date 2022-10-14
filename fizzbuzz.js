4144a004f17c7525428c4e604d8c3f7f8d11a114
viet anh nguyen <nguyenvietanh262001@gmail.com>
2022-10-14 21:17:26 +0700

    pass test case number 5

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,12 +1,21 @@
 const FIZZ = 'fizz'
+const BUZZ = 'buzz'
 
 function fizzbuzz (number) {
   if (this.isFizz(number)) {
     return FIZZ
+  }
+
+  if (this.isBuzz(number)) {
+    return BUZZ
   }
   return number
 }
 
 function isFizz (number) {
   return number === 3
+}
+
+function isBuzz (number) {
+  return number === 5
 }
